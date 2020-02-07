1be8e32e225b0edb69d9686099ad801dc8d006a4
mangoALCATRAZ <angeluccimatt4@gmail.com>
2020-02-07 02:57:24 -0500

    typo fix

--- src/desencryptedchat/DESencryptedChat.java
+++ src/desencryptedchat/DESencryptedChat.java
@@ -117,7 +117,7 @@ public class DESencryptedChat {
                     
                 }
                 
-                System.out.print("Me: " + userInput + "\n");
+                System.out.print("Me: " + userInput + "/n");
                 
                 // ENCRYPTION GOES HERE. SEND OUT ENCRYPTED CIPHERTEXT INSTEAD
                 // OF userInput
