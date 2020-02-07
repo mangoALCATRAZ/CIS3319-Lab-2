b759de022b3a04d00b00cf79090143022404555a
Jack Tilley <47064848+Jack-Tilley@users.noreply.github.com>
2020-02-07 14:07:27 -0500

    added updates

---
+++ listenerThread.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package desencryptedchat;
+
+import java.net.*;
+import java.io.*;
+/**
+ *
+ * @author woah dude
+ */
+public class listenerThread extends Thread{
+    private Socket sock;
+    private volatile boolean endFlag;
+    
+    public listenerThread(Socket inSock){
+        sock = inSock;
+        endFlag = false;
+    }
+    
+    @Override
+    public void run() {
+        try{
+            BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+            
+            System.out.println("\nListener Thread Ready.");
+            
+            while(!endFlag){
+                String received = in.readLine(); //incoming ciphertext
+//                System.out.println("received cyphertext: " + received);
+                
+                // DECRYPTION GOES HERE, PRINT OUT RESULTING PLAINTEXT INSTEAD OF received
+                
+                // we might need to allow input of a normal key below
+                String[] ReversedRoundKeyArray = KeyGenerator.keyGenerator(KeyGenerator.key);
+                
+                ReversedRoundKeyArray = KeyGenerator.roundKeyArrayReversal(ReversedRoundKeyArray);
+                String pt = EncryptDecrypt.Decrypt(received, ReversedRoundKeyArray);
+                String printOut = ChatHelper.binaryStringToText(pt);
+                
+                
+                System.out.println(sock.getInetAddress().toString() + ": " + printOut); // output goes here.
+                
+                
+            }
+            
+            sock.close();
+            System.out.println("Server-aspect done running.");
+
+            
+        }
+        catch(IOException e){
+            
+            System.out.print("\nListener thread: " + e);
+            
+            
+        }
+   }
+    
+    // called by other thread
+    public void end(){
+        endFlag = true;
+    }
+}
+    
+
